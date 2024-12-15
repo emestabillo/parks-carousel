bc925552d91dd07290c7bd1d831f1193437d1e0a
Emmilie Estabillo <emsestabillo@gmail.com>
2024-12-15 09:32:54 -0500

    Initial commit

---
+++ src/data/parks.js
@@ -0,0 +1,22 @@
+export const parksData = [
+  {
+    headline: "Magic Kingdom",
+    link: "https://disneyworld.disney.go.com/destinations/magic-kingdom/",
+    src: "https://images.unsplash.com/photo-1576419535009-bb544190a306?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    headline: "EPCOT",
+    link: "https://disneyworld.disney.go.com/destinations/epcot",
+    src: "https://images.unsplash.com/photo-1553526630-81e8f0d6ffde?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    headline: "Animal Kingdom",
+    link: "https://disneyworld.disney.go.com/destinations/animal-kingdom/",
+    src: "https://images.unsplash.com/photo-1692303366685-390f3e039bf9?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    headline: "Hollywood Studios",
+    link: "https://disneyworld.disney.go.com/destinations/hollywood-studios/",
+    src: "https://images.unsplash.com/photo-1708881641022-43a3b8e82532?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
