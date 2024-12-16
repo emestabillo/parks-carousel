3653c3ecc7d108734937c84fe3c41473488d7e2d
Emmilie Estabillo <emsestabillo@gmail.com>
2024-12-15 23:34:19 -0500

    Add transition, upgrade to react 19

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import styles from "./Button.module.scss";
 
-function Button({ href, children, className = "", ...props }) {
+function Button({ href, children, className = "", ref, ...props }) {
   const ButtonTag = href ? "a" : "button";
   return (
     <ButtonTag
       className={`${styles.button} ${className}`}
       {...(href ? { href } : {})}
+      ref={ref}
       {...props}
     >
       {children}
--- src/components/carousel/Slide.jsx
+++ src/components/carousel/Slide.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import styles from "./Carousel.module.scss";
 import Button from "../Button/Button";
 
 function Slide({ headline, link, src, index, activeSlideIndex }) {
+  const textContainerRef = useRef(null);
+  const buttonRef = useRef(null);
   const isActive = index === activeSlideIndex;
+
+  useEffect(() => {
+    if (index === activeSlideIndex) {
+      gsap.fromTo(
+        textContainerRef.current,
+        { opacity: 0, y: 20 },
+        { opacity: 1, y: 0, duration: 1 }
+      );
+      gsap.fromTo(
+        buttonRef.current,
+        { opacity: 0, y: 20 },
+        { opacity: 1, y: 0, duration: 0.8 },
+        ">-.7"
+      );
+    }
+  }, [index, activeSlideIndex]);
   return (
     <div
       key={headline}
@@ -11,8 +31,10 @@ function Slide({ headline, link, src, index, activeSlideIndex }) {
       tabIndex={isActive ? -1 : undefined}
     >
       <div className={styles.textContainer}>
-        <h3 className={styles.headline}>{headline}</h3>
-        <Button href={link} target="_blank">
+        <h3 className={styles.headline} ref={textContainerRef}>
+          {headline}
+        </h3>
+        <Button href={link} target="_blank" ref={buttonRef}>
           learn more
         </Button>
       </div>
