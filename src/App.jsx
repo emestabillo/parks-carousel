04e608b169fdb5263b1d24acc148921892b2d4d0
Emmilie Estabillo <emsestabillo@gmail.com>
2024-12-16 12:22:13 -0500

    Add carousel files back

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Carousel from "./components/Carousel/Carousel";
+
 function App() {
   return (
     <main>
       <h1 className="screenReaderOnly">Disney World Destinations</h1>
+      <Carousel />
     </main>
   );
 }
